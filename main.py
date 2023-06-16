f6e6d29964bf1613e69f41f0a9131c266bae8de4
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-06-16 21:45:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def view_delete():
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
     # flask --app main run --host=0.0.0.0 --port=8000 --debug
 
     # 1. Поднять фласк,
